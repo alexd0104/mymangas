484e504a8f1059136ceb0ee5031df449dccd2822
Alexandre <alexandre.decavele@gmail.com>
2025-10-05 10:21:24 +0200

    ajout des premières entités

---
+++ src/Entity/Bibliotheque.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BibliothequeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BibliothequeRepository::class)]
+class Bibliotheque
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    /**
+     * @var Collection<int, Manga>
+     */
+    #[ORM\OneToMany(targetEntity: Manga::class, mappedBy: 'bibliotheque')]
+    private Collection $mangas;
+
+    public function __construct()
+    {
+        $this->mangas = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Manga>
+     */
+    public function getMangas(): Collection
+    {
+        return $this->mangas;
+    }
+
+    public function addManga(Manga $manga): static
+    {
+        if (!$this->mangas->contains($manga)) {
+            $this->mangas->add($manga);
+            $manga->setBibliotheque($this);
+        }
+
+        return $this;
+    }
+
+    public function removeManga(Manga $manga): static
+    {
+        if ($this->mangas->removeElement($manga)) {
+            // set the owning side to null (unless already changed)
+            if ($manga->getBibliotheque() === $this) {
+                $manga->setBibliotheque(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Manga.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MangaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MangaRepository::class)]
+class Manga
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $serie = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $tome = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\ManyToOne(inversedBy: 'mangas')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Bibliotheque $bibliotheque = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSerie(): ?string
+    {
+        return $this->serie;
+    }
+
+    public function setSerie(string $serie): static
+    {
+        $this->serie = $serie;
+
+        return $this;
+    }
+
+    public function getTome(): ?int
+    {
+        return $this->tome;
+    }
+
+    public function setTome(?int $tome): static
+    {
+        $this->tome = $tome;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBibliotheque(): ?Bibliotheque
+    {
+        return $this->bibliotheque;
+    }
+
+    public function setBibliotheque(?Bibliotheque $bibliotheque): static
+    {
+        $this->bibliotheque = $bibliotheque;
+
+        return $this;
+    }
+}
