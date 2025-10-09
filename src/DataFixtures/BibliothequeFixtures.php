f7d0bbc77a1df5f6187740ba67af05973e397237
Alexandre <alexandre.decavele@gmail.com>
2025-10-09 10:31:15 +0200

    fixtures plus réalistes

--- src/DataFixtures/BibliothequeFixtures.php
+++ src/DataFixtures/BibliothequeFixtures.php
@@ -6,23 +6,50 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Bibliotheque;
 
-
 class BibliothequeFixtures extends Fixture
 {
-
+    // Compatibilité avec l'existant : pointe vers "Bibliothèque de Alexandre" (la 1ʳᵉ)
     public const REF_BIBLIO_DEMO = 'biblio_demo';
+
+    // Références explicites (utiles depuis d'autres fixtures)
+    public const REF_BIBLIO_ALEXANDRE = 'biblio_alexandre';
+    public const REF_BIBLIO_QUENTIN = 'biblio_quentin';
+    public const REF_BIBLIO_JEREMY      = 'biblio_jeremy';
+    public const REF_BIBLIO_NICOLAS     = 'biblio_nicolas';
+    public const REF_BIBLIO_LAURA       = 'biblio_laura';
 
     public function load(ObjectManager $manager): void
     {
-        $biblio = new Bibliotheque();
-        $biblio->setTitre('Bibliothèque de Démo');
+        $prenoms = ['Alexandre', 'Alexandre', 'Jeremy', 'Nicolas', 'Laura'];
+        $refs    = [
+            self::REF_BIBLIO_ALEXANDRE,
+            self::REF_BIBLIO_QUENTIN,
+            self::REF_BIBLIO_JEREMY,
+            self::REF_BIBLIO_NICOLAS,
+            self::REF_BIBLIO_LAURA,
+        ];
 
-        $manager->persist($biblio);
+        $first = null;
+
+        foreach ($prenoms as $i => $prenom) {
+            $biblio = new Bibliotheque();
+            $biblio->setTitre('Bibliothèque de ' . $prenom);
+
+            $manager->persist($biblio);
+
+            // Ajouter des références utilisables par d'autres fixtures
+            $this->addReference($refs[$i], $biblio);
+
+            if ($i === 0) {
+                $first = $biblio; // "Bibliothèque de Alexandre" (1ʳᵉ)
+            }
+        }
+
         $manager->flush();
 
-        $this->addReference(self::REF_BIBLIO_DEMO, $biblio);
-
-
+        // Référence legacy pour compatibilité (ex: utilisée par MangaFixtures)
+        if ($first) {
+            $this->addReference(self::REF_BIBLIO_DEMO, $first);
+        }
     }
-    
 }
--- src/DataFixtures/MangaFixtures.php
+++ src/DataFixtures/MangaFixtures.php
@@ -3,42 +3,77 @@
 namespace App\DataFixtures;
 
 use App\Entity\Manga;
+use App\Entity\Bibliotheque;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\DataFixtures\BibliothequeFixtures;
-use App\Entity\Bibliotheque ;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-
-
-class MangaFixtures extends Fixture 
+class MangaFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $biblio = $this->getReference(BibliothequeFixtures::REF_BIBLIO_DEMO, Bibliotheque::class);
-
-        $rows = [
-            ['One Piece', 'One Piece', 1],
-            ['One Piece', 'One Piece', 2],
-            ['One Piece', 'One Piece', 3],
-            ['Dragon Ball', 'Dragon Ball', 1],
-            ['Dragon Ball', 'Dragon Ball', 2],
-            ['Dragon Ball', 'Dragon Ball', 3],
-            ['Death Note', 'Death Note', 1],
-            ['Death Note', 'Death Note', 2],
-            ['SNK', 'SNK', 1],
-            ['Frieren', 'Frieren', 1],
+        // Pool de "bons mangas" (on varie les séries)
+        // Format: [titre, serie, tome]
+        $pool = [
+            ['One Piece',            'One Piece',            1],
+            ['Naruto',               'Naruto',               1],
+            ['Bleach',               'Bleach',               1],
+            ['Dragon Ball',          'Dragon Ball',          1],
+            ['Fullmetal Alchemist',  'Fullmetal Alchemist',  1],
+            ['Attack on Titan',      'Attack on Titan',      1], // SNK
+            ['Demon Slayer',         'Demon Slayer',         1],
+            ['Jujutsu Kaisen',       'Jujutsu Kaisen',       1],
+            ['My Hero Academia',     'My Hero Academia',     1],
+            ['Chainsaw Man',         'Chainsaw Man',         1],
+            ['Vinland Saga',         'Vinland Saga',         1],
+            ['Spy x Family',         'Spy x Family',         1],
+            ['Tokyo Ghoul',          'Tokyo Ghoul',          1],
+            ['Haikyuu!!',            'Haikyuu!!',            1],
+            ['Frieren',              'Frieren',              1],
         ];
 
-        foreach ($rows as [$titre, $serie, $tome]) {
-            $m = new Manga();
-            $m->setTitre($titre);
-            $m->setSerie($serie);
-            $m->setTome($tome);
-            $m->setBibliotheque($biblio);
+        // Références (depuis BibliothequeFixtures)
+        $libRefs = [
+            BibliothequeFixtures::REF_BIBLIO_ALEXANDRE,
+            BibliothequeFixtures::REF_BIBLIO_QUENTIN,
+            BibliothequeFixtures::REF_BIBLIO_JEREMY,
+            BibliothequeFixtures::REF_BIBLIO_NICOLAS,
+            BibliothequeFixtures::REF_BIBLIO_LAURA,
+        ];
 
-            $manager->persist($m);
+        // 10 mangas par bibliothèque, en prenant des tranches décalées du pool pour varier
+        $perLib = 10;
+        $poolCount = count($pool);
+
+        foreach ($libRefs as $i => $refName) {
+            /** @var Bibliotheque $biblio */
+            $biblio = $this->getReference($refName, Bibliotheque::class);
+
+            for ($k = 0; $k < $perLib; $k++) {
+                // Décalage pour varier les séries selon la bibliothèque
+                $index = ($i * 3 + $k) % $poolCount;
+                [$titre, $serie, $tome] = $pool[$index];
+                $tome = random_int(1, 10);
+
+
+                $m = new Manga();
+                $m->setTitre($titre);
+                $m->setSerie($serie);
+                $m->setTome($tome);
+                $m->setBibliotheque($biblio);
+
+                $manager->persist($m);
+            }
         }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        // Assure que BibliothequeFixtures est chargée avant celle-ci
+        return [
+            BibliothequeFixtures::class,
+        ];
     }
 }
